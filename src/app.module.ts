f4865a72823396fe683eeb5d77330abf0a9fb9b0
Jorge Monge <jlo4321jl@gmail.com>
2022-04-12 06:34:23 -0600

    Feat/prisma (#10)
    * ➕ Add prisma as dev dependency
    
    * ➕ Add @prisma/client dependency
    
    * 🙈 Add .env file to .gitignore
    
    * 🗃️ Add User schema to schema.prisma file
    
    * 🗃️ Add User migration
    
    * 🔥 Delete app unused files
    
    * ➕ Add @nestjs/config dependency
    
    * ➕ Add joi dependency
    
    * 🔧 Add config.ts file
    
    * 🔧 Add config module in App module
    
    * ✨ Create Prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { PrismaModule } from './prisma/prisma.module';
+import config from './config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [config],
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    PrismaModule,
+  ],
 })
 export class AppModule {}
