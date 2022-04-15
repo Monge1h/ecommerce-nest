a99d39ad7e9f5962691a62b812ca0189ad0076e6
Jorge Monge <jlo4321jl@gmail.com>
2022-04-14 19:41:06 -0600

    Feat/products (#15)
    * ✨ Add module products
    
    * ✨ Add utils module
    
    * 🔧 Add products and utils module to app module
    
    * ➕ Add geoip-lite dependency
    
    * ✨ Add GeoLocationMiddleware
    
    * ✨ Implement GeoLocationMiddleware on main.ts file
    
    * ✨ Add userPreferecesMiddleware
    
    * ✨ Implement userPreferencesMiddleware on app.module.ts file
    
    * ✨ Use userPreferences to show data base on user location
    
    * 🗃️ Order products by international_shipping
    
    * ✨ Sort products by user country

---
+++ src/products/products.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Response,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  async findAll(@Response() res) {
+    const user_preferences = res.locals.user_preferences;
+    const data = await this.productsService.findAll(user_preferences);
+    return res.status(200).json(data);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
