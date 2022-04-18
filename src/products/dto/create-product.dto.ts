39d38d2094f5ea8f80c2d461af10211e84efc09e
Jorge Monge <jlo4321jl@gmail.com>
2022-04-17 21:53:27 -0600

    ✨ Add dtos for product

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  readonly name: string;
+  readonly sku: string;
+  readonly id_category: number;
+  readonly id_country: number;
+  readonly international_shipping: boolean;
+  readonly id_language_description_product: number;
+  readonly description_product: string;
+  readonly id_curency: number;
+  readonly price: number;
+}
