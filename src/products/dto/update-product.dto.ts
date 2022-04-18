cff8115abbd827d031b903b5d4fa555b8b78e9ff
Jorge Monge <jlo4321jl@gmail.com>
2022-04-18 01:29:43 -0600

    🏷️ Add types of product dtos

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,7 +1,28 @@
+import { IsString, IsNotEmpty, IsNumber, IsBoolean } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class UpdateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'products name' })
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'sku' })
   readonly sku: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'category' })
   readonly id_category: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'country' })
   readonly id_country: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  @ApiProperty({ description: 'international shipping' })
   readonly international_shipping: boolean;
 }
